9d3ff48077ea1d98eb869b17a7cf1f416e8b0179
dawndevil <kevinkiller@gmail.com>
2017-07-30 12:17:34 +0800

    fix the keyboard bug

--- command.go
+++ command.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/nsf/termbox-go"
+import (
+	"github.com/nsf/termbox-go"
+
+)
 
 const (
 	MOVE = iota + 1000
@@ -14,17 +17,26 @@ type Command struct {
 
 func ListenKeyEvent(c chan Command) {
 	termbox.SetInputMode(termbox.InputEsc)
-
+	//上一次按键记录
+	var record termbox.Key 
+	//本次按键命令
+	var command Command
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
 			switch(ev.Key) {
-			case termbox.KeyArrowUp: c <- Command{command : MOVE, key : ev.Key}
-			case termbox.KeyArrowDown: c <- Command{command : MOVE, key : ev.Key}
-			case termbox.KeyArrowRight: c <- Command{command : MOVE, key : ev.Key}
-			case termbox.KeyArrowLeft: c <- Command{command : MOVE, key : ev.Key}
-			case termbox.KeyCtrlC : c <- Command{command : END, key : ev.Key}
+			case termbox.KeyArrowUp: command = Command{command : MOVE, key : ev.Key}
+			case termbox.KeyArrowDown: command = Command{command : MOVE, key : ev.Key}
+			case termbox.KeyArrowRight: command = Command{command : MOVE, key : ev.Key}
+			case termbox.KeyArrowLeft: command = Command{command : MOVE, key : ev.Key}
+			case termbox.KeyCtrlC : command = Command{command : END, key : ev.Key}
 			}
+		}
+
+		//判断是否一直重复按一个键
+		if(command.key != record) {
+			record = command.key
+			c <- command
 		}
 	}
 }
